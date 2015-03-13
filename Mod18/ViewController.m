73729adfeff31021f186adf240fad01f6690010a
cachenew <krunal@MacBookPro.local>
2015-03-13 13:15:49 +0530

    final

--- Mod18/ViewController.m
+++ Mod18/ViewController.m
@@ -8,11 +8,48 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@import MessageUI;
+
+@interface ViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
 @implementation ViewController
+
+-(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    
+    switch (result) {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Cancelled");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Failed");
+        case MFMailComposeResultSaved:
+            NSLog(@"Saved");
+        case MFMailComposeResultSent:
+            NSLog(@"Sent");
+        default:
+            NSLog(@"Default");
+            break;
+    }
+    
+    [controller dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if(NO == [MFMailComposeViewController canSendMail])
+    {
+        NSLog(@"Cannot send mail");
+        return;
+    }
+    
+    MFMailComposeViewController *vc = [[MFMailComposeViewController alloc] init];
+    vc.mailComposeDelegate = self;
+    
+    [self presentViewController:vc animated:YES completion:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
